91acc778284418b30471a7ca8322a1c0e79d0454
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2023-09-18 19:47:43 +0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./pages/components/Footer";
 import "./App.css";
 import HomePage from "./pages/HomePage";
@@ -8,14 +10,18 @@ import About from "./pages/About";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <HomePage />
-      {/* <NotFound /> */}
-      {/* <About /> */}
-      {/* <Projects /> */}
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 

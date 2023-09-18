903a8e3429c8f6afd6dafb6c15575f63840ba183
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2023-09-18 18:30:11 +0300

    Projects page and footer and navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import Footer from "./pages/components/Footer";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import Projects from "./pages/Projects";
+import Navbar from "./pages/components/Navbar";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Navbar />
+      {/* <HomePage /> */}
+      {/* <NotFound /> */}
+      <Projects />
+      <Footer />
     </div>
   );
 }
 
-export default App; 
+export default App;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,82 +4,6 @@ import MyImage from "../assets/img/myImage.jpg";
 function HomePage() {
   return (
     <div>
-      <nav class="bg-white border-gray-200 dark:bg-gray-800">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" class="flex items-center">
-            <img src={Logo} alt="Logo" className="mr-3 h-6 sm:h-9 w-2/3" />
-          </a>
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span class="sr-only">Open main menu</span>
-            <svg
-              class="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-800">
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-white bg-primary-700 rounded md:bg-transparent md:text-primary-700 md:p-0 dark:text-white md:dark:text-primary-500"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Tech Stack
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Projects
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
       <section class="bg-white dark:bg-gray-900">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="mr-auto place-self-center lg:col-span-7">
@@ -95,7 +19,15 @@ function HomePage() {
             <img class="rounded-lg shadow-lg" src={MyImage} alt="mockup" />
           </div>
         </div>
-        <div class="grid max-w-screen-xl grid-cols-2 gap-8 px-4 pb-8 mx-auto text-gray-500 lg:pb-16 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
+        <div class="max-w-2xl mx-auto text-center">
+          <h2 class="text-3xl font-extrabold leading-tight tracking-tight text-gray-900 sm:text-4xl dark:text-white">
+            Custom works
+          </h2>
+          <p class="mt-4 text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400">
+            Things I’ve built so far
+          </p>
+        </div>
+        <div class="grid mt-12 max-w-screen-xl grid-cols-2 gap-8 px-4 pb-8 mx-auto text-gray-500 lg:pb-16 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
           <a href="#" class="flex justify-center">
             <i class="devicon-nextjs-original colored xl:text-8xl text-7xl hover:opacity-100 opacity-90 dark:hover:text-white"></i>
           </a>
@@ -562,64 +494,6 @@ function HomePage() {
           </div>
         </div>
       </section>
-      <footer class="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
-        <div class="mx-auto max-w-screen-xl text-center">
-          <a
-            href="#"
-            class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white"
-          >
-          <img src={Logo} alt="" />
-          </a>
-          <p class="my-6 text-gray-500 dark:text-gray-400">
-            Open-source library of over 400+ web components and interactive
-            elements built for better web.
-          </p>
-          <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6 ">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
-                Premium
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6 ">
-                Campaigns
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
-                Blog
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
-                Affiliate Program
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
-                FAQs
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
-                Contact
-              </a>
-            </li>
-          </ul>
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2021-2022{" "}
-            <a href="#" class="hover:underline">
-              Flowbite™
-            </a>
-            . All Rights Reserved.
-          </span>
-        </div>
-      </footer>
     </div>
   );
 }
